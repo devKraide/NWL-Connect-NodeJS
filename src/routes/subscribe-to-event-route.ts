f729cf0e74748f075c601dfc26b26cab202ab773
Nicolas <nicolas.kraide@gamil.com>
2025-02-21 23:43:22 -0300

    creating first functionality

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,36 +1,37 @@
-import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import {z} from "zod";
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { subscribeToEvent } from '../functions/subscribe-to-event'
 
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribe users to an event',
+        tags: ['Subscription'],
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
 
-export const subscribeToEventRoute:FastifyPluginAsyncZod = async (app) => {
-
-app.post("/subscriptions", {
-  schema: {
-    summary: "Subscribe users to an event",
-    tags: ['Subscription'], 
-    body: z.object({
-      name: z.string(),
-      email: z.string().email(),
-    }),
-
-    response: {
-      201: z.object({
-        name: z.string(),
-        email: z.string().email(),
-      }),
+        response: {
+          201: z.object({
+            subscriberId: z.string(),
+          }),
+        },
+      },
     },
+    async (request, reply) => {
+      const { name, email } = request.body
 
-  },
-}, async (request, reply) => {
-  const { name, email } = request.body
+      const { subscriberId } = await subscribeToEvent({
+        name,
+        email,
+      })
 
-  return reply.status(201)
-    .send({
-      name,
-      email
-    })
-  })
-
-
-
+      return reply.status(201).send({
+        subscriberId,
+      })
+    }
+  )
 }
